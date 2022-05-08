3f3c5b4c8a90569ce446ad75ddf7f1917060dc2b
sefhirot69 <josalillo@gmail.com>
2022-05-08 22:18:56 +0200

    feat : 🟥 tests FakerPostRepositoryTest.php

---
+++ Backend/tests/Minerva/Posts/Infrastructure/FakerPostRepositoryTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Minerva\Posts\Infrastructure;
+
+use Minerva\Posts\Domain\Post;
+use Minerva\Posts\Infrastructure\FakerPostRepository;
+use PHPUnit\Framework\TestCase;
+
+final class FakerPostRepositoryTest extends TestCase
+{
+    /** @test */
+    public function itShouldReturnPostsDomain(): void
+    {
+        $fakerPost = new FakerPostRepository();
+
+        $result = $fakerPost->findAll();
+
+        foreach ($result as $post) {
+            self::assertInstanceOf(Post::class, $post);
+        }
+    }
+
+}
