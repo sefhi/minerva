b427d9c9aaa9a9a1165bdaa03db21b7abd6b124c
sefhirot69 <josalillo@gmail.com>
2022-05-09 20:33:29 +0200

    feat : 🟩 Valido el valueObject Name

--- Backend/Minerva/Shared/Domain/ValueObject/Primitive/StringValueObject.php
+++ Backend/Minerva/Shared/Domain/ValueObject/Primitive/StringValueObject.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Minerva\Shared\Domain\ValueObject\Primitive;
 
+use InvalidArgumentException;
+
 abstract class StringValueObject
 {
     public function __construct(protected string $value)
@@ -13,5 +15,15 @@ abstract class StringValueObject
     public function value(): string
     {
         return $this->value;
+    }
+
+    protected function checkLength(int $minLength, int $maxLength): bool
+    {
+        $length = strlen($this->value());
+        if ($length < $minLength || $length > $maxLength) {
+            throw new InvalidArgumentException(sprintf('%s must have a length between %s and %s character.', static::class, $minLength, $maxLength), 400);
+        }
+
+        return true;
     }
 }
---
+++ Backend/tests/Minerva/Shared/Domain/ValueObject/NameTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Minerva\Tests\Shared\Domain\ValueObject;
+
+use App\Tests\Minerva\Shared\Domain\MotherCreator;
+use InvalidArgumentException;
+use Minerva\Shared\Domain\ValueObject\Name;
+use PHPUnit\Framework\TestCase;
+
+class NameTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider providerNamesValid
+     */
+    public function itShouldCreateName(string $value): void
+    {
+        $name = new Name($value);
+        self::assertEquals($value, $name->value());
+    }
+
+    /**
+     * @test
+     * @dataProvider providerNamesLengthInvalid
+     */
+    public function itShouldThrowAndExceptionWhenNameLengthIsNotValid(string $value): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectErrorMessage(Name::class.' must have a length between 3 and 50 character.');
+
+        $name = new Name($value);
+    }
+
+    /**
+     * @test
+     * @dataProvider providerNamesInValid
+     */
+    public function itShouldThrowAndExceptionWhenNameIsNotValid(string $value): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectErrorMessage($value.' is not valid in '.Name::class);
+
+        new Name($value);
+    }
+
+    private function providerNamesLengthInvalid(): array
+    {
+        return [
+            ['hi'],
+            ['hihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihihi'],
+            ['h'],
+        ];
+    }
+
+    private function providerNamesValid(): array
+    {
+        return [
+            ['Pepe Huevo Frito'],
+            ['Dolores del Ano'],
+            ['Testículo de Jehova'],
+            ['Álvaro Morán'],
+            ['Dr.Mongolo'],
+            ['Jose María Nuñez'],
+            [MotherCreator::random()->firstName().' '.MotherCreator::random()->lastName()],
+            [MotherCreator::random()->firstName().' '.MotherCreator::random()->lastName()],
+            [MotherCreator::random()->firstName().' '.MotherCreator::random()->lastName()],
+        ];
+    }
+
+    private function providerNamesInValid(): array
+    {
+        return [
+            ['_Pepito_'],
+            ['D0l0r3s del 4n0'],
+            ['T3stìculo de Jehova'],
+            ['1234'],
+            ['Têst näs'],
+            ['xxXS3fhiXxx'],
+        ];
+    }
+}
