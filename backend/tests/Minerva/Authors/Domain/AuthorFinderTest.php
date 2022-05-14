147d212eb2fb9ae4b8b6344bec6b5ba13f960c5b
sefhirot69 <josalillo@gmail.com>
2022-05-14 12:12:23 +0200

    feat : 🟩 creo el test para el servicio de dominio, que devuelve un author dado un id

---
+++ backend/tests/Minerva/Authors/Domain/AuthorFinderTest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Minerva\Authors\Domain;
+
+use Minerva\Authors\Domain\AuthorRepository;
+use Minerva\Shared\Domain\Exceptions\AuthorNotFoundException;
+use Minerva\Tests\Shared\Domain\ValueObject\Author\AuthorIdMother;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+final class AuthorFinderTest extends TestCase
+{
+    private MockObject $repository;
+
+    protected function setUp(): void
+    {
+        $this->repository = $this->createMock(AuthorRepository::class);
+    }
+
+    /** @test */
+    public function itShouldReturnAndAuthor(): void
+    {
+        // Given
+        $authorId = AuthorIdMother::random();
+        $author = AuthorMother::create($authorId);
+
+        $this->repository
+            ->expects(self::once())
+            ->method('find')
+            ->with($authorId)
+            ->willReturn($author);
+
+        $authorFinder = new AuthorFinder($this->repository);
+
+        // When
+        $result = ($authorFinder)($authorId);
+
+        // Then
+        self::assertSame($author, $result);
+    }
+
+    /** @test */
+    public function itShouldThrowAndAuthorNotFoundExceptionWhenNotFindAuthor(): void
+    {
+        // Then
+        $this->expectExceptionCode(AuthorNotFoundException::class);
+        $this->expectExceptionCode(404);
+
+        // Given
+        $authorId = AuthorIdMother::random();
+
+        $this->repository
+            ->expects(self::once())
+            ->method('find')
+            ->with($authorId)
+            ->willReturn(null);
+
+        $authorFinder = new AuthorFinder($this->repository);
+
+        // When
+        ($authorFinder)($authorId);
+    }
+
+
+}
