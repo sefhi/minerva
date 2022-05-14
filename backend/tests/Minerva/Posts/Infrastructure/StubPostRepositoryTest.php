5d86aecb849328fc859e47529e0f64b8c4b52816
sefhirot69 <josalillo@gmail.com>
2022-05-14 13:43:55 +0200

    feat : 🟥 cambio los tests StubPostRepositoryTest.php

--- backend/tests/Minerva/Posts/Infrastructure/StubPostRepositoryTest.php
+++ backend/tests/Minerva/Posts/Infrastructure/StubPostRepositoryTest.php
@@ -4,24 +4,36 @@ declare(strict_types=1);
 
 namespace App\Tests\Minerva\Posts\Infrastructure;
 
+use App\Tests\Minerva\Authors\Domain\AuthorMother;
+use Minerva\Authors\Domain\AuthorFinder;
 use Minerva\Posts\Domain\Post;
-use Minerva\Posts\Domain\PostAuthorNotFoundException;
 use Minerva\Posts\Infrastructure\StubPostRepository;
+use Minerva\Shared\Domain\Exceptions\AuthorNotFoundException;
 use Minerva\Shared\Domain\ValueObject\Author\AuthorId;
 use Minerva\Tests\Posts\Domain\Dto\PostCreatorDtoMother;
 use Minerva\Tests\Posts\Domain\PostContentMother;
 use Minerva\Tests\Posts\Domain\PostTitleMother;
-use Minerva\Tests\Shared\Domain\MotherCreator;
-use Minerva\Tests\Shared\Domain\ValueObject\Author\AuthorIdMother;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 final class StubPostRepositoryTest extends TestCase
 {
+    private MockObject $authorFinderMock;
+
+    protected function setUp(): void
+    {
+        $this->authorFinderMock = $this->createMock(AuthorFinder::class);
+    }
 
     /** @test */
     public function itShouldReturnPostDomainWhenCallFunctionFindAll(): void
     {
-        $stubRepository = new StubPostRepository();
+        $this->authorFinderMock
+            ->expects(self::once())
+            ->method('__invoke')
+            ->willReturn(AuthorMother::create());
+
+        $stubRepository = new StubPostRepository($this->authorFinderMock);
 
         $result = $stubRepository->findAll();
 
@@ -33,8 +45,12 @@ final class StubPostRepositoryTest extends TestCase
     /** @test */
     public function itShouldSavePostWhenCallFunctionSave(): void
     {
+        $this->authorFinderMock
+            ->expects(self::never())
+            ->method('__invoke');
+
         $postCreatorDto = PostCreatorDtoMother::random();
-        $stubRepository = new StubPostRepository();
+        $stubRepository = new StubPostRepository($this->authorFinderMock);
 
         $result = $stubRepository->save($postCreatorDto);
 
@@ -42,20 +58,28 @@ final class StubPostRepositoryTest extends TestCase
     }
 
     /** @test */
-    public function itShouldThrowAPostAuthorNotFoundExceptionWhenSeachByAuthorIdThatNotExists(): void
+    public function itShouldThrowAAuthorNotFoundExceptionWhenSearchByAuthorIdThatNotExists(): void
     {
-        $this->expectException(PostAuthorNotFoundException::class);
-        $this->expectExceptionCode(404);
-
+        // Given
+        $authorId = new AuthorId(60);
         $postCreatorDto = PostCreatorDtoMother::create(
             PostTitleMother::random(),
             PostContentMother::random(),
-            new AuthorId(60)
+            $authorId
         );
 
-        $stubRepository = new StubPostRepository();
+        $this->authorFinderMock
+            ->expects(self::once())
+            ->method('__invoke')
+            ->willThrowException(new AuthorNotFoundException($authorId->value()));
+
+        $stubRepository = new StubPostRepository($this->authorFinderMock);
+
+        // Then
+        $this->expectException(AuthorNotFoundException::class);
+        $this->expectExceptionCode(404);
+
+        // When
         $stubRepository->save($postCreatorDto);
     }
-
-
 }
