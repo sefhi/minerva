8fa8c80e49846f3124f6ab6633032017921c0c65
josealiaslloret <josalillo@gmail.com>
2022-07-19 18:02:23 +0200

    Update: workflow codestyle and refactor tests functional with mock of the repositories

--- atenea/tests/Controller/Posts/PostsFindAllGetControllerTest.php
+++ atenea/tests/Controller/Posts/PostsFindAllGetControllerTest.php
@@ -2,21 +2,33 @@
 
 namespace App\Tests\Controller\Posts;
 
+use Atenea\Posts\Domain\PostRepository;
+use Atenea\Tests\Posts\Domain\PostMother;
+use PHPUnit\Framework\MockObject\MockObject;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class PostsFindAllGetControllerTest extends WebTestCase
 {
     private KernelBrowser $client;
+    private MockObject|PostRepository $postRepositoryMock;
 
     protected function setUp(): void
     {
         $this->client = self::createClient();
+        $this->postRepositoryMock = $this->createMock(PostRepository::class);
     }
 
-    /** @test */
+    /** @test
+     * @throws \Exception
+     */
     public function whenCallEndpointPostAllShouldReturnAllPost(): void
     {
+        $this->postRepositoryMock->expects(self::once())->method('findAll')->willReturn(PostMother::array());
+
+
+        self::getContainer()->set(PostRepository::class, $this->postRepositoryMock);
+
         $this->client->request(
             'GET',
             '/posts/all'
