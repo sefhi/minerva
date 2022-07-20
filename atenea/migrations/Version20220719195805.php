f1c8dd0e5adbeccb653e559625baa572e0689d85
josealiaslloret <josalillo@gmail.com>
2022-07-20 17:25:47 +0200

    Update: add migrations && other fixes

--- atenea/migrations/Version20220719195805.php
+++ atenea/migrations/Version20220719195805.php
@@ -22,32 +22,33 @@ final class Version20220719195805 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(
             'CREATE TABLE author
-  (
-     id       INT auto_increment NOT NULL,
-     name     VARCHAR(50) NOT NULL,
-     username VARCHAR(70) NOT NULL,
-     website  VARCHAR(255) NOT NULL,
-     email    VARCHAR(255) NOT NULL,
-     PRIMARY KEY(id)
-  )
-DEFAULT CHARACTER SET utf8mb4
-COLLATE `utf8mb4_unicode_ci`
-engine = innodb'
+                  (
+                     id       INT auto_increment NOT NULL,
+                     name     VARCHAR(50) NOT NULL,
+                     username VARCHAR(70) NOT NULL,
+                     website  VARCHAR(255) NOT NULL,
+                     email    VARCHAR(255) NOT NULL,
+                     PRIMARY KEY(id)
+                  )
+                DEFAULT CHARACTER SET utf8mb4
+                COLLATE `utf8mb4_unicode_ci`
+                engine = innodb'
         );
 
         $this->addSql(
             'CREATE TABLE post
-  (
-     id        INT auto_increment NOT NULL,
-     author_id INT NOT NULL,
-     title     VARCHAR(255) NOT NULL,
-     content   VARCHAR(255) NOT NULL,
-     INDEX post_author_id_fk (author_id),
-     PRIMARY KEY(id)
-  )
-DEFAULT CHARACTER SET utf8mb4
-COLLATE `utf8mb4_unicode_ci`
-engine = innodb '
+                  (
+                     id        INT auto_increment NOT NULL,
+                     author_id INT NOT NULL,
+                     title     VARCHAR(255) NOT NULL,
+                     content   VARCHAR(255) NOT NULL,
+                     INDEX post_author_id_fk (author_id),
+                     PRIMARY KEY(id),
+                     FOREIGN KEY (author_id) REFERENCES author(id)
+                  )
+                DEFAULT CHARACTER SET utf8mb4
+                COLLATE `utf8mb4_unicode_ci`
+                engine = innodb '
         );
     }
 
