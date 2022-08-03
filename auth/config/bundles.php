7c6065f57629d149054c1c78854d27dbe30aa825
josealiaslloret <josalillo@gmail.com>
2022-08-03 20:19:54 +0200

    Update: delete package league_oauth2_server.yaml

--- auth/config/bundles.php
+++ auth/config/bundles.php
@@ -6,5 +6,4 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    League\Bundle\OAuth2ServerBundle\LeagueOAuth2ServerBundle::class => ['all' => true],
 ];
