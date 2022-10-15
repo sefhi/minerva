ecc7ba55edef9a35d91165dd06f38795b886a43b
Jose Ma Alias <josemariaalias@ipglobal.es>
2022-10-15 22:11:30 +0200

    Feat: Valid Url

--- auth/Auth/Clients/Domain/ClientRedirectUris.php
+++ auth/Auth/Clients/Domain/ClientRedirectUris.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Auth\Clients\Domain;
 
+use InvalidArgumentException;
+
 final class ClientRedirectUris
 {
     public function __construct(private array $values)
     {
+        $this->ensureIsValidUrl($values);
     }
 
     /**
@@ -17,4 +20,16 @@ final class ClientRedirectUris
     {
         return $this->values;
     }
+
+    private function ensureIsValidUrl(array $values): void
+    {
+        foreach ($values as $value) {
+            if (!filter_var($value, FILTER_VALIDATE_URL)) {
+                throw new InvalidArgumentException(
+                    sprintf('The redirectUris <%s> is not valid', $value)
+                );
+            }
+        }
+    }
+
 }
