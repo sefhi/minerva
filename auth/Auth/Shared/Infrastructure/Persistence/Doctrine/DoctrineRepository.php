c2b2313e3ffc20e4c8cdd3461f5e13b02b134c2a
Jose Ma Alias <josemariaalias@ipglobal.es>
2022-10-15 11:54:52 +0200

    Feat: Abstract class shared for doctrine

---
+++ auth/Auth/Shared/Infrastructure/Persistence/Doctrine/DoctrineRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Auth\Shared\Infrastructure\Persistence\Doctrine;
+
+use Auth\Shared\Domain\Aggregate\AggregateRoot;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+
+abstract class DoctrineRepository
+{
+
+    public function __construct(private readonly EntityManagerInterface $entityManager)
+    {
+    }
+
+    protected function entityManager(): EntityManagerInterface
+    {
+        return $this->entityManager;
+    }
+
+    protected function persist(AggregateRoot $entity): void
+    {
+        $this->entityManager()->persist($entity);
+        $this->entityManager()->flush($entity);
+    }
+
+    protected function remove(AggregateRoot $entity): void
+    {
+        $this->entityManager()->remove($entity);
+        $this->entityManager()->flush($entity);
+    }
+
+    protected function repository(string $entityClass): EntityRepository
+    {
+        return $this->entityManager->getRepository($entityClass);
+    }
+}
