d6f0cb8a088d0b0c47b2595e81738a0abc9171ee
Jose Ma Alias <josemariaalias@ipglobal.es>
2022-10-16 01:13:43 +0200

    Feat: Add command

--- auth/Auth/Clients/Application/Create/CreateClientCommand.php
+++ auth/Auth/Clients/Application/Create/CreateClientCommand.php
@@ -4,7 +4,55 @@ declare(strict_types=1);
 
 namespace Auth\Clients\Application\Create;
 
+use Auth\Clients\Domain\ClientGrants;
+use Auth\Clients\Domain\ClientName;
+use Auth\Clients\Domain\ClientRedirectUris;
+use Auth\Clients\Domain\ClientScopes;
+
 final class CreateClientCommand
 {
 
+    private function __construct(
+        private ClientName $name,
+        private ClientGrants $grants,
+        private ?ClientRedirectUris $redirectUris = null,
+        private ?ClientScopes $scopes = null,
+        private bool $active = true,
+    ) {
+    }
+
+    public static function create(
+        ClientName $name,
+        ClientGrants $grants,
+        ?ClientRedirectUris $redirectUris = null,
+        ?ClientScopes $scopes = null,
+        bool $active = true,
+    ): self {
+        return new self($name, $grants, $redirectUris, $scopes, $active);
+    }
+
+    public function getName(): ClientName
+    {
+        return $this->name;
+    }
+
+    public function getGrants(): ClientGrants
+    {
+        return $this->grants;
+    }
+
+    public function getRedirectUris(): ?ClientRedirectUris
+    {
+        return $this->redirectUris;
+    }
+
+    public function getScopes(): ?ClientScopes
+    {
+        return $this->scopes;
+    }
+
+    public function isActive(): bool
+    {
+        return $this->active;
+    }
 }
--- auth/Auth/Clients/Application/Create/CreateClientCommandHandler.php
+++ auth/Auth/Clients/Application/Create/CreateClientCommandHandler.php
@@ -4,7 +4,18 @@ declare(strict_types=1);
 
 namespace Auth\Clients\Application\Create;
 
+use Auth\Clients\Domain\Client;
+use Auth\Clients\Domain\ClientSaveRepository;
+
 final class CreateClientCommandHandler
 {
 
+    public function __construct(private ClientSaveRepository $repository)
+    {
+    }
+
+    public function __invoke(CreateClientCommand $command)
+    {
+//        $this->repository->save();
+    }
 }
