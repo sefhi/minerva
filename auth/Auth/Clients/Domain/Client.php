87ec37f5ffe9d81539be9df9c76948f32554b714
Jose Ma Alias <josemariaalias@ipglobal.es>
2022-10-15 12:20:57 +0200

    Feat: Mapping first level client

--- auth/Auth/Clients/Domain/Client.php
+++ auth/Auth/Clients/Domain/Client.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace Auth\Clients\Domain;
 
+use Auth\Shared\Domain\Aggregate\AggregateRoot;
 use Ramsey\Uuid\UuidInterface;
 
-final class Client
+final class Client extends AggregateRoot
 {
     public function __construct(
-        private UuidInterface $id,
+        private readonly UuidInterface $id,
         private ClientCredentialsParam $credentials,
         private ?ClientRedirectUris $redirectUris = null,
         private ?ClientGrants $grants = null,
@@ -19,53 +20,34 @@ final class Client
     {
     }
 
-    /**
-     * @return UuidInterface
-     */
     public function getId(): UuidInterface
     {
         return $this->id;
     }
 
-    /**
-     * @return ClientCredentialsParam
-     */
     public function getCredentials(): ClientCredentialsParam
     {
         return $this->credentials;
     }
 
-    /**
-     * @return ClientRedirectUris|null
-     */
     public function getRedirectUris(): ?ClientRedirectUris
     {
         return $this->redirectUris;
     }
 
-    /**
-     * @return ClientGrants|null
-     */
     public function getGrants(): ?ClientGrants
     {
         return $this->grants;
     }
 
-    /**
-     * @return ClientScopes|null
-     */
     public function getScopes(): ?ClientScopes
     {
         return $this->scopes;
     }
 
-    /**
-     * @return bool
-     */
     public function isActive(): bool
     {
         return $this->active;
     }
-
 
 }
