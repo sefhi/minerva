25cb6ad1f8d1b746fb7f99bf3c2f44c138950388
sefhirot69 <josalillo@gmail.com>
2022-11-16 19:20:41 +0100

    Refactor: change to create ssh key

--- auth/.docker/genrsa.sh
+++ auth/.docker/genrsa.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-if [ ! -f .docker/config/private.key ]; then
-  openssl genrsa -out .docker/config/private.key 2048;
-  chmod 660 .docker/config/private.key;
+
+BASE_URL=$(pwd)
+PRIVATE_KEY_FILE=$BASE_URL/.docker/config/private.key
+PUBLIC_KEY_FILE=$BASE_URL/.docker/config/public.key
+
+if [ ! -f "$PRIVATE_KEY_FILE" ]; then
+  echo 'Creating private key';
+  openssl genrsa -out "$PRIVATE_KEY_FILE" 2048;
+  chmod 660 "$PRIVATE_KEY_FILE";
 fi
 
-if [ ! -f .docker/config/public.key ]; then
-  openssl rsa -in .docker/config/private.key -pubout -out .docker/config/public.key;
-  chmod 660 .docker/config/public.key;
+if [ ! -f "$PUBLIC_KEY_FILE" ]; then
+  echo 'Creating public key';
+  openssl rsa -in "$PRIVATE_KEY_FILE" -pubout -out "$PUBLIC_KEY_FILE";
+  chmod 660 "$PUBLIC_KEY_FILE";
 fi
